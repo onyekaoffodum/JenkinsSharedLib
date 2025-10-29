a92f00fee28ccaa838f785de4f1fb580ae539781
onyekaoffodum <sandieji@rocketmail.com>
2025-10-29 07:40:22 +0100

    Added sendSlackNofications file

---
+++ vars/sendSlackNotifications.groovy
@@ -0,0 +1,7 @@
+def call (String channel, String color, String message){
+def sendSlack(String buildStatus,String channel) {
+    def color = buildStatus == 'SUCCESS' ? 'good' : (buildStatus == 'UNSTABLE' ? 'warning' : 'danger')
+    def message = "*${buildStatus}*: Job `${env.JOB_NAME}` - Build #${env.BUILD_NUMBER} \n" +
+                  "Details: ${env.BUILD_URL}console"
+    }
+}
