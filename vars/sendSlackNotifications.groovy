89b877bfa36f249280fd09934483037f57891122
onyekaoffodum <sandieji@rocketmail.com>
2025-10-29 07:48:43 +0100

    Fixed issues with sendSlackNotofications file

--- vars/sendSlackNotifications.groovy
+++ vars/sendSlackNotifications.groovy
@@ -1,7 +1,5 @@
-def call (String channel, String color, String message){
-def sendSlack(String buildStatus,String channel) {
+def call (String buildStatus, String channel){
     def color = buildStatus == 'SUCCESS' ? 'good' : (buildStatus == 'UNSTABLE' ? 'warning' : 'danger')
     def message = "*${buildStatus}*: Job `${env.JOB_NAME}` - Build #${env.BUILD_NUMBER} \n" +
                   "Details: ${env.BUILD_URL}console"
     }
-}
